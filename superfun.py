61ef1568e6a8b39e6e31f7d4d4ce32316c2c2300
【★༒✞︎Îτß-ℓєgєи∂-ℓx】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-09-25 05:45:35 -0700

    Update superfun.py

--- superfun.py
+++ superfun.py
@@ -4,7 +4,7 @@ import asyncio
 async def fuckd(ult):
  await ult.edit("fuck")
  await asyncio.sleep(2)
- await ult.edit("society")
+ await ult.edit("Legend-Lx")
  await asyncio.sleep(2)
  await ult.edit("fuck") 
  await asyncio.sleep(2)
